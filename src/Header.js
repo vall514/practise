eeb5ab468eab4ba6b01ecfee0ebf8f6af621ca1e
vall514 <valentineachieng46@gmail.com>
2025-05-21 11:53:28 +0300

    updated header with welcome to our event

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <header style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white', textAlign: 'center' }}>
-      <h1>🎉 Welcome to Practice Event 🎉</h1>
+      <h1>🎉 Welcome to Practice Event 🎉 you all</h1>
+      <h2>welcome to our event</h2>
       <p>📅 June 2025 | 📍 Pwani University</p>
       <p>You are invited to attend the big event that will be held at pwani university play grounds</p>
     </header>
