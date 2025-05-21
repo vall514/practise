103be5138d2391cc7ccc87eb767ee1162bea799c
vall514 <valentineachieng46@gmail.com>
2025-05-21 10:20:13 +0300

    add Header component and update app

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white', textAlign: 'center' }}>
+      <h1>🎉 Welcome to Practice Event 🎉</h1>
+      <p>📅 June 2025 | 📍 Pwani University</p>
+      <p>You are invited to attend the big event that will be held at pwani university play grounds</p>
+    </header>
+  );
+}
+
+export default Header;
